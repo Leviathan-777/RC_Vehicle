c3f0ecd314ddf070295f68add000b6b5769347bc
Leviathan-777 <mbeclawski@gmail.com>
2025-12-06 13:01:39 +0100

    Initial commit

---
+++ main/main.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+#include "freertos/FreeRTOS.h"
+#include "freertos/task.h"
+#include "nvs_flash.h"
+#include "esp_err.h"
+#include "esp_log.h"
+
+static const char *TAG = "APP_MAIN";
+
+void app_main(void)
+{
+    // ---- Initialize NVS ----
+    esp_err_t ret = nvs_flash_init();
+    if (ret == ESP_ERR_NVS_NO_FREE_PAGES || ret == ESP_ERR_NVS_NEW_VERSION_FOUND) 
+    {
+        ESP_ERROR_CHECK(nvs_flash_erase());
+        ret = nvs_flash_init();
+    }
+    ESP_ERROR_CHECK(ret);
+    ESP_LOGI(TAG, "NVS initialized successfully");
+
+    while (1) 
+    {
+        vTaskDelay(pdMS_TO_TICKS(1000)); // Delay for 1 second
+    }
+}
